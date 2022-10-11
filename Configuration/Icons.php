d114c48dfd4d88f42a671eed51c85bc5a1a21997
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2022-10-11 11:52:54 +0200

    [TASK] Register icons via Icons.php for TYPO3 v11

---
+++ Configuration/Icons.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'ext-matomo_optout-ce' => [
+        'provider' => TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:' . Brotkrueml\MatomoOptout\Extension::KEY . '/Resources/Public/Icons/content-element.svg',
+    ],
+];
--- ext_tables.php
+++ ext_tables.php
@@ -2,15 +2,17 @@
 
 defined('TYPO3') || die();
 
-(function () {
-    $iconRegistry = TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
-        TYPO3\CMS\Core\Imaging\IconRegistry::class
-    );
-    $iconRegistry->registerIcon(
-        'ext-matomo_optout-ce',
-        TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        ['source' => 'EXT:' . Brotkrueml\MatomoOptout\Extension::KEY . '/Resources/Public/Icons/content-element.svg']
-    );
+(static function () {
+    if ((new TYPO3\CMS\Core\Information\Typo3Version())->getMajorVersion() < 11) {
+        $iconRegistry = TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
+            TYPO3\CMS\Core\Imaging\IconRegistry::class
+        );
+        $iconRegistry->registerIcon(
+            'ext-matomo_optout-ce',
+            TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+            ['source' => 'EXT:' . Brotkrueml\MatomoOptout\Extension::KEY . '/Resources/Public/Icons/content-element.svg']
+        );
+    }
 
     TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
         '@import "EXT:' . Brotkrueml\MatomoOptout\Extension::KEY . '/Configuration/TSconfig/Page/NewContentElementWizard.tsconfig"'
